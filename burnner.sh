e9e3a8070a9f6e2c3590f245c8f195130a3dcf78
Amir Khazaie <733amir@gmail.com>
2017-01-05 18:28:19 +0330

    Make eclipse project for autocompletion and other things with an IDE.

--- burnner.sh
+++ burnner.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+cd micro-lab-project/Source
+
 avr-gcc -w -Os -DF_CPU=1000000UL -mmcu=atmega32 -c -o main.o main.c
 avr-gcc -w -mmcu=atmega32 main.o -o main
 avr-objcopy -O ihex -R .eeprom main main.hex
--- micro-lab-project/Source/main.c
+++ micro-lab-project/Source/main.c
@@ -1,18 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
- 
-int main(void) {
 
+int main() {
 	DDRD = 0xff;
 
-	while(1){
-
+	while (1) {
+		_delay_ms(25);
 		PORTD = 0xff;
-		_delay_ms(1000);
 
+		_delay_ms(25);
 		PORTD = 0x00;
-		_delay_ms(1000);
 	}
-
 	return 0;
 }
