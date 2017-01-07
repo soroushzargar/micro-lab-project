f2662e47a48a5740d0fb34b983d37576384e9167
Amir Khazaie <733amir@gmail.com>
2017-01-07 14:45:21 +0330

    Adding mode, Timer and get motions with interrupt.

--- micro-lab-project/Source/main.c
+++ micro-lab-project/Source/main.c
@@ -6,10 +6,15 @@
 
 // ------------------------- Global Variables Definitions -------------------------
 unsigned char nums[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-char time[17];
+char data[17];
 
 uint8_t second, minute, hour;
 uint16_t day;
+
+unsigned char mode = 0x00;  // Alarm mode
+//unsigned char mode = 0xFF;  // Auto lighting
+
+uint8_t mov_det_count;
 // ------------------------- Global Variables Definitions -------------------------
 
 
@@ -23,27 +28,17 @@ void lcd_char_at(uint8_t x, uint8_t y, uint8_t data) {
 	lcd_data(data);
 }
 
-void initialization() {
-	DDRB = 0x00;  // Input for PIR sensor.
-
-	DDRC = 0xFF;
-	PORTC = 0xFF;
-
-	lcd_init();
-
-	timer1_init();
-
-	sei();  // Enable global interrupt.
-}
-
-// initialize timer, interrupt and variable
 void timer1_init() {
     TCCR1B |= (1 << CS12) | (1 << CS10) | (1 << WGM12);  // set up timer with prescaler = 1024
     TCNT1 = 0;
-    OCR1A = 969;
+    OCR1A = 968;
     TIMSK |= (1 << OCIE1A);  // enable compare match interrupt
 
     second = minute = hour = day = 0;
+
+    // Viewing on LCD.
+	sprintf(data, "Time:%2u %2.2u:%2.2u:%2.2u", day, hour, minute, second);
+	lcd_str_at(0, 1, data);
 }
 
 ISR(TIMER1_COMPA_vect) {
@@ -54,8 +49,50 @@ ISR(TIMER1_COMPA_vect) {
 	day += hour / 24;             hour %= 24;
 
 	// Viewing on LCD.
-	sprintf(time, "Time:%2u %2.2u:%2.2u:%2.2u", day, hour, minute, second);
-	lcd_str_at(0, 1, time);
+	sprintf(data, "Time:%2u %2.2u:%2.2u:%2.2u", day, hour, minute, second);
+	lcd_str_at(0, 1, data);
+
+	// PIR Sensor Status
+	if ((PINB & (1 << PB2)) == (1 << PB2))
+		lcd_str_at(13, 3, "Yes");
+	else
+		lcd_str_at(13, 3, "No!");
+}
+
+void ext_int2_init() {
+	// Interrupt on rising edge
+	MCUCSR |= (1 << ISC2);
+
+	GIMSK |= (1 << INT2); // Interrupt to activate INT2
+	GICR |= (1 << INT2); // Activate interrupt to INT2
+
+	mov_det_count = -1;
+
+	sprintf(data, "Num of Mov: %4u", ++mov_det_count);
+	lcd_str_at(0, 4, data);
+}
+
+ISR(INT2_vect) {
+	_delay_ms(1);  // To catch interrupts that we don't want.
+
+	sprintf(data, "Num of Mov: %4u", ++mov_det_count);
+	lcd_str_at(0, 4, data);
+}
+
+void initialization() {
+	DDRB = 0x00;  // Input for PIR sensor to INT2.
+	PORTB = 0xFF;
+
+	DDRC = 0xFF;
+	PORTC = 0xFF;
+
+	lcd_init();
+
+	timer1_init();
+
+	ext_int2_init();
+
+	sei();  // Enable global interrupt.
 }
 
 int main() {
@@ -69,22 +106,7 @@ int main() {
 
 	initialization();
 
-	while (1) {
-		// ------------------------- LED Blinking PORTC -------------------------
-//		PORTC = 0xFF;
-//		_delay_ms(50);
-//
-//		PORTC = 0x00;
-//		_delay_ms(50);
-//		// ------------------------- LED Blinking PORTC -------------------------
-//
-//
-//		// ------------------------- PIR Sensor -------------------------
-//		if ((PINB & (1 << PB0)) == (1 << PB0))
-//			lcd_str_at(0, 4, "Yes");
-//		else
-//			lcd_str_at(0, 4, "No!");
-		// ------------------------- PIR Sensor -------------------------
-	}
+	while (1);
+
 	return 0;
 }
